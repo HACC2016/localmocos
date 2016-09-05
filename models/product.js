7c6e3d23be22cf25d7c060152c5655dc40ce93f5
javenkn <javenkn7@yahoo.com>
2016-09-04 22:36:10 -1000

    removed image association since we are not using more than one image per product (for now)

--- models/product.js
+++ models/product.js
@@ -13,7 +13,6 @@ module.exports = function(sequelize, DataTypes) {
       associate: function(models) {
         // associations can be defined here
         models.Product.belongsTo(models.VenderInfo);
-        models.Product.hasMany(models.Image);
         models.Product.hasMany(models.Certification);
       }
     }
