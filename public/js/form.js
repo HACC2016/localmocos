dc9fa843d2782858aaa59f71ec8857953adb14ef
Nikki K <808nikz@gmail.com>
2016-09-14 21:39:32 -1000

    added a onchange function

--- public/js/form.js
+++ public/js/form.js
@@ -1,9 +1,7 @@
-alert("form.js");
-
 function getOther (val) {
   var element=document.getElementById('other');
 
-  if(val === 'Other') {
+  if(val === "Other") {
     element.style.display='block';
   } else {
     element.style.display='none';
