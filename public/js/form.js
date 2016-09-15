33fb51f6de0b282d71c36b19162b29896aae33ef
Nikki K <808nikz@gmail.com>
2016-09-14 21:10:54 -1000

    Trying to create onChange for form.pug

--- public/js/form.js
+++ public/js/form.js
@@ -1,1 +1,11 @@
 alert("form.js");
+
+function getOther (val) {
+  var element=document.getElementById('other');
+
+  if(val === 'Other') {
+    element.style.display='block';
+  } else {
+    element.style.display='none';
+  }
+}
