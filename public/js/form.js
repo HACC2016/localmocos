9ccc26daaf724b4a16c1766a94e74cf22901a690
Nikki K <808nikz@gmail.com>
2016-09-15 18:22:44 -1000

    made changes

--- public/js/form.js
+++ public/js/form.js
@@ -1,10 +1,19 @@
-var elements = $('select');
-elements.on('change', function (event) {
+var elements = $("select");
+elements.on("change", function (event) {
   console.log(event);
-  var input = $(this).next('.other').find('input');
+  var input = $(this).next("div.other").find("input");
   if(event.target.value === "Other") {
-    input.css('display', 'block');
+    input.css("visibility", "visible");
   } else {
-    input.css('display', 'none');
+    input.css("display", "none");
   }
-})
+});
+
+// elements.change(function (event) {
+//   var input = $(this).find("input").each();
+//   if(event.target.value === "Other"){
+//     input.show();
+//   } else {
+//     input.hide();
+//   }
+// });
