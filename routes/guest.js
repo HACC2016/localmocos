2483ffab04327a6da86d8c85a0ad93851a6bba1b
Nikki K <808nikz@gmail.com>
2016-09-15 19:16:35 -1000

    testing

--- routes/guest.js
+++ routes/guest.js
@@ -60,7 +60,10 @@ app.get('/',function(req, res, next) {
     res.render('index', {subtitle: "Find Local Business & Products"})
 });
 
-
+////// testing ///////
+app.get('/about', function (req, res) {
+  res.render('about');
+});
 
 
 
