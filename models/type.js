83040d9271bc3042c4dc81bbbbab0fa455621f81
Spacekins <spacekins@zoho.com>
2016-09-15 22:18:52 -1000

    adding type again

--- models/type.js
+++ models/type.js
@@ -33,6 +33,7 @@ module.exports = function(sequelize, DataTypes) {
           type: sequelize.QueryTypes.SELECT
         });
 
+        /** Please push up **/
         return searchResultData;
       }
     }
