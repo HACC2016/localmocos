e034d9bb01d3d1d497347f693d4c0595cbf3f515
Nikki K <808nikz@gmail.com>
2016-09-16 01:01:31 -1000

    testing the edit

--- routes/seller.js
+++ routes/seller.js
@@ -52,14 +52,42 @@ module.exports = function(express, app, path, bodyParser, querystring, db) {
     app.get(/seller\/\d+\/edit$/, function(req, res) {
       // console.log(db.Type.findAll());
       // 
-      
-    db.Type.findAll({})
+    
+    // db.Type.findAll({})
+    // .then((data) => {
+    //   console.log(data);
+    //   res.render('editVendorForm', {
+    //                 methodType: 'PUT',
+    //                 actionType: '/seller/{id}',
+    //                 formTitle: 'Edit Seller'
+    //                  }
+    //             });
+    // });
+
+    db.VendorInfo.findOne({
+        where: {
+            id: cleanParamMiddle(req.url,2)
+        }
+    })
     .then((data) => {
       console.log(data);
       res.render('editVendorForm', {
                     methodType: 'PUT',
                     actionType: '/seller/{id}',
-                    formTitle: 'Edit Seller'
+                    formTitle: 'Edit Seller',
+                    vendor: {
+                        user: data.user_id,
+                        company: data.company_name,
+                        business_reg_name: data.business_reg_name,
+                        business_description: data.business_description,
+                        dba: data.dba,
+                        address1: data.address1,
+                        address2: data.address2,
+                        business_ph: data.business_ph,
+                        sales_ph: data.sales_ph,
+                        website: data.website,
+                        email: data.email
+                    }
                 });
     });
                 
