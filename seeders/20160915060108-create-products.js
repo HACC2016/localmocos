d5d002833ed438d8bc278bb8414b48a4e6049a93
javenkn <javenkn7@yahoo.com>
2016-09-15 21:00:22 -1000

    added another big island candy to products

--- seeders/20160915060108-create-products.js
+++ seeders/20160915060108-create-products.js
@@ -6,7 +6,7 @@ module.exports = {
     {
       name: 'Hawaiian Macadamia Nuts',
       description: 'Truly a classic bite-size snack, these 100% Hawaii-grown whole macadamia nuts are dry-roasted and lightly salted to bring out their rich flavor.',
-      product_info_id: 2,
+      product_info_id: 6,
       vendor_info_id: 1,
       price: 12.50,
       qty: 1000,
@@ -14,7 +14,19 @@ module.exports = {
       isActive: true,
       createdAt: new Date(),
       updatedAt: new Date()
-    }
+    },
+    {
+      name: 'Milk Chocolate Covered Hawaiian Macadamia Nuts',
+      description: 'drops of deliciousness feature premium milk chocolate and 100% pure Hawaiian macadamia nuts.',
+      product_info_id: 6,
+      vendor_info_id: 1,
+      price: 11.75,
+      qty: 2000,
+      image: 'https://bic-bigislandcandies.netdna-ssl.com/media/catalog/product/cache/1/image/320x/9df78eab33525d08d6e5fb8d27136e95/1/1/1100macnutmilk_0641.jpg',
+      isActive: true,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
     ], {})
   },
 
