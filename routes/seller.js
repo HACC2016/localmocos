84346f3910837e92f793c6f6c453af0ccf456105
Bryce <bsaito88@gmail.com>
2016-09-15 22:13:39 -1000

    Added to POST handler to validate zip code with the city and island.

--- routes/seller.js
+++ routes/seller.js
@@ -24,26 +24,50 @@ module.exports = function(express, app, path, bodyParser, querystring, db) {
     /**** New Seller Form *****/
 
     app.get('/seller/new', function(req, res) {
-      db.VendorInfo.findAll({
-        include: [
-          {
-            model: db.Certification,
-            required: true
-          }
-        ]
-      })
-      .then((stuff) => {
-        return res.json(stuff);
-      })
-      // res.render('vendorForm');
+      // db.VendorInfo.findAll({
+      //   include: [
+      //     {
+      //       model: db.Service,
+      //       required: true
+      //     }
+      //   ]
+      // })
+      // .then((stuff) => {
+      //   return res.json(stuff);
+      // })
+      return res.render('vendorForm');
     });
 
-    app.post('/seller/new', function(req, res) {
-      res.render('vendorForm', {
-          methodType: 'POST',
-          actionType: '/seller/new',
-          formTitle: 'Create New Seller'
-      });
+    app.post('/seller', function(req, res, next) {
+      var locals = req.body;
+      var city = '';
+      var island = '';
+      db.Zipcode.findAll({
+        where: {
+          zip: req.body.zip
+        }
+      })
+      .then((data) => {
+        var dataValues = data[0].dataValues;
+        city = dataValues.city;
+        island = dataValues.island;
+        if (city === locals.city && island === locals.island) {
+          res.send('OK!');
+        } else {
+          console.log(city, locals.city, island, locals.island)
+          throw new TypeError('City and Island do not match')
+        }
+        console.log(city, island);
+      })
+      // .then(() => {
+      //   VendorInfo.create({
+      //     company_name: locals.companyName,
+      //     dba: locals.dba,
+      //     bus_reg_num: locals.regNum,
+      //     address1: locals.addressOne,
+      //     address2: locals.addressTwo,
+      //   })
+      // })
     });
 
     app.get(/seller\/\d+\/edit$/, function(req, res) {
