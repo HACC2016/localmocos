69ec3a4133401ae295ed1cc8c8c09d574ea5cb99
Bryce <bsaito88@gmail.com>
2016-09-21 23:42:59 -1000

    Set value to null when Other checkbox is checked.

--- public/js/form.js
+++ public/js/form.js
@@ -3,12 +3,13 @@ $(function () {
   elements.change(function (event) {
     var input = $(this).parent().siblings(".other");
     if(event.target.parentElement.innerText.toLowerCase() === "other") {
+      event.target.value = null;
       if(this.checked===true){
         input.css({"visibility": "visible", "display": "block"}).attr("disabled", false);
         } else {
           input.css({"visibility": "none", "display": "none"}).attr("disabled", true);
         }
-        
+
       }
   });
 })
