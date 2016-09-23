755f897447d23ac258223f3027e5593011749df6
javenkn <javenkn7@yahoo.com>
2016-09-22 20:27:06 -1000

    added getVendorCheckboxInfo function

--- models/vendorinfo.js
+++ models/vendorinfo.js
@@ -47,6 +47,19 @@ module.exports = function(sequelize, DataTypes) {
           foreignKey: 'zip_id',
           targetKey: 'id'
         });
+      },
+      getVendorCheckboxInfo: function (vendor_id) {
+        var getTypeInfos = "SELECT vendor_infos.id, type_id, job, 'type' specific_info FROM vendor_infos JOIN vendor_info_types ON vendor_infos.id=vendor_info_id JOIN types ON types.id=type_id WHERE vendor_infos.id="  + vendor_id;
+        var getServiceInfos = "SELECT vendor_infos.id, service_id, service, 'service' specific_info FROM vendor_infos JOIN vendor_info_services ON vendor_infos.id=vendor_info_id JOIN services ON services.id=service_id WHERE vendor_infos.id=" + vendor_id;
+        var getMarketInfos = "SELECT vendor_infos.id, market_id, market, 'market' specific_info FROM vendor_infos JOIN vendor_info_markets ON vendor_infos.id=vendor_info_id JOIN markets ON markets.id=market_id WHERE vendor_infos.id=" + vendor_id;
+        var getCertificationInfos = "SELECT vendor_infos.id, cert_id, specialty, 'certification' specific_info FROM vendor_infos JOIN vendor_info_certs ON vendor_infos.id=vendor_info_id JOIN certifications ON certifications.id=cert_id WHERE vendor_infos.id=" + vendor_id + ";";
+        var union = ' UNION ';
+
+        var checkBoxInfo = sequelize.query(getTypeInfos + union + getServiceInfos + union + getMarketInfos + union + getCertificationInfos, {
+          type: sequelize.QueryTypes.SELECT
+        });
+
+        return checkBoxInfo;
       }
     }
   });
